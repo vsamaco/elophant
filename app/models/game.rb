dd47d77d7e09dc304e74102e418c31e4c42c76de
Vincent Samaco <vsamaco@gmail.com>
2013-01-03 22:38:35 -0800

    Added function for converting date object to iso for games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,11 @@
 class Game < ActiveRecord::Base
   attr_accessible :game_id, :map_id, :queue_type, :sub_type, :game_type, :create_date, :players, :game_statistics
   has_and_belongs_to_many :players
-  has_many :game_statistics, :foreign_key => :game_id, :primary_key => :game_id
+  has_many :game_statistics, :foreign_key => :game_id, :primary_key => :game_id, :dependent => :destroy
+  
+  def date_to_iso(date)
+    milliseconds = date[/Date\((\d+)\)/][$1].to_i
+    timestamp = Time.at(milliseconds/1000)
+    DateTime.parse(timestamp.to_s).to_s
+  end
 end
