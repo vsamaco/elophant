871c1f62c290455ea09b6454afd25fae7234a0e7
Vincent Samaco <vsamaco@gmail.com>
2013-01-05 18:14:48 -0800

    Added player stat to summary view

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,21 @@
 class Player < ActiveRecord::Base
-  attr_accessible :account_id, :summoner_id, :summoner_name, :region, :games, :game_statistics
+  attr_accessible :account_id, :summoner_id, :summoner_name, :region, :games, :game_statistics, :player_statistics
   
   has_and_belongs_to_many :games
   has_many :game_statistics
+  has_many :player_statistics
   
   scope :active, where("account_id IS NOT NULL")
   
   def summary
-    summary_recent_games
+    summary = {
+      :recent_games => summary_recent_games,
+      :player_stat => summary_player_stat
+    }
+  end
+  
+  def summary_player_stat
+    self.player_statistics.ranked_solo.first
   end
   
   def summary_recent_games
