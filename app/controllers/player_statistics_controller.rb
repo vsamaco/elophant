c8a6933cf7e2149595eaec80f0a526d11965f7cb
Vincent Samaco <vsamaco@gmail.com>
2013-01-05 18:14:17 -0800

    Created player statistics index view

--- app/controllers/player_statistics_controller.rb
+++ app/controllers/player_statistics_controller.rb
@@ -1,4 +1,10 @@
 class PlayerStatisticsController < ApplicationController
   def index
+    @player_statistics = PlayerStatistic.all
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @player_statistics }
+    end
   end
 end
